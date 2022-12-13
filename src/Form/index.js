35884d58ed87253d2dc917a43b02eb72f9506056
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-13 18:36:00 +0100

    edited onFormSubmit - added new var and if

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,8 +6,14 @@ const Form = ({addNewTask}) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
+    const trimmedNewTaskContent = newTaskContent.trim();
+
+    if (! trimmedNewTaskContent){
+      return;
+    }
+    addNewTask(trimmedNewTaskContent);
     setNewTaskContent("");
+
   };
 
     return (
