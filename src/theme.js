84f0ab05e3a7bee981bcee89fdbca00ab6763002
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-20 00:06:05 +0100

    added new colors

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,9 @@ export const theme = {
         darkTeal: "hsl(180, 100%, 20%)",
         lightTeal: "hsl(180, 44%, 61%)",
         lightGrey: "rgb(223, 223, 223)",
+        green: "rgb(255, 255, 255)",
+        lightGreen: "hsl(120, 96%, 40%)",
+        veryLightGreen: "hsl(120, 96%, 50%)",
     },
     breakpoints: {
         mobile: 767
