9cd6c620b37b620a8a04ec847cfe0d4aadc0ffa7
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-20 00:08:52 +0100

    edited green color

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ export const theme = {
         darkTeal: "hsl(180, 100%, 20%)",
         lightTeal: "hsl(180, 44%, 61%)",
         lightGrey: "rgb(223, 223, 223)",
-        green: "rgb(255, 255, 255)",
+        green: "hsl(120, 96%, 33%)",
         lightGreen: "hsl(120, 96%, 40%)",
         veryLightGreen: "hsl(120, 96%, 50%)",
     },
