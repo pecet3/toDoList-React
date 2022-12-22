29098c6635de60d0fbe2bc0ae9d138b5980d1c46
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-22 23:07:33 +0100

    added ThemeProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {theme} from "./theme.js";
+import {ThemeProvider} from "styled-components"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
