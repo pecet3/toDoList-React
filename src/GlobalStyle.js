275d80db03359d43503b8e072d37813be480b462
Jakub Pacewicz <pecet3107@gmail.coim>
2022-12-22 23:11:37 +0100

    created GlobalStyle, moved index.css there

---
+++ src/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+
+    }
+
+    *,::after,::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        word-break: break-word;
+        font-family: 'Lato', sans-serif;
+        max-width: 800px;
+        margin: 0 auto;
+        text-align: left;
+        background-color: rgb(223, 223, 223);
+        text-overflow: clip;
+        padding: 10px;
+    };
+
+`;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {theme} from "./theme.js";
-import {ThemeProvider} from "styled-components"
+import { theme } from "./theme.js";
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
