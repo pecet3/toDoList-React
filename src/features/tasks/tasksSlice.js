2b54631dca5f1e5f7005c7b27763f476c9dff022
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-01 18:17:54 +0100

    configured slice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,14 +4,30 @@ const tasksSlice = createSlice({
     name: "tasks",
     initialState: {
         tasks: [],
+        hideDone: false,
     },
     reducers: {
         addTask: ({ tasks }, { payload }) => {
             tasks.push(payload);
         },
-    },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: ({ tasks }, { payload: task}) => {
+            const index = tasks.findIndex(({ id }) => id === task);
+            tasks[index].done = !tasks[index].done;
+        },
+        setAllTasksDone: ({ tasks }) => {
+            tasks.forEach((task) => task.done = true);
+        },
+        removeTask: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks.splice(index,1);
+        },
+    }
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, setAllTasksDone, removeTask } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
+export const selectHideDone = state => selectTasks(state).hideDone;
 export default tasksSlice.reducer;
