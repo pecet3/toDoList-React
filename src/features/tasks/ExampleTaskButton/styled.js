acf2f8d2b81d5a2d4e53a4ff2d99bc31eea78ca5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-03 00:14:01 +0100

    styled Button

--- src/features/tasks/ExampleTaskButton/styled.js
+++ src/features/tasks/ExampleTaskButton/styled.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-    padding: 10px;
-    background-color: ${({ theme }) => theme.colors.teal};
+    padding: 7px;
+    background-color: ${({ theme }) => theme.colors.lightGrey};
     font-size: 14px;
     border: none;
-    color: white;
-    transition: 600ms;
+    color: black;
+    transition: .3s;
     
     &:hover{
         cursor: pointer;
+        filter: brightness(120%);
+        transform: scale(1.03);
     };
-  
-    `;
-export const Wrapper = styled.div`
-    display:flex;
-`    
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+        margin-top: 5px;
+    }
+    `; 
