5259540b8629b325a8fce7675d0283b80ccbe2c9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-02 20:07:33 +0100

    added redux elements

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -1,9 +1,22 @@
 import { StyledButton } from './styled';
+import { selectIsAllDone, removeAllTasks, selectTasksLenght } from '../../features/tasks/tasksSlice';
+import { useSelector, useDispatch } from "react-redux";
 
-const Button = () => (
-    <>
-        <StyledButton>kliknij</StyledButton>
-    </>
-);
+const Button = () => {
+    const taskLenght = useSelector(selectTasksLenght)
+    const isAllDone = useSelector(selectIsAllDone);
+    const dispatch = useDispatch();
+    return (
+        <>
+            <StyledButton
+                changeBackground={!isAllDone}
+                onClick={()=> dispatch(removeAllTasks())}
+                lenght={taskLenght}
+            >
+                kliknij
+            </StyledButton>
+        </>
+    )
+};
 
 export default Button;
