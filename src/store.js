1c48845bc23ed466f8045792a777c99b4dc7f861
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-02 23:52:23 +0100

    added saga to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import tasksReducer from "./features/tasks/tasksSlice";
+import { watchFetchExampleTasks } from "./features/tasks/tasksSaga";
 
-export default configureStore({
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         tasks: tasksReducer,
-    }
-})
+    },
+    middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(watchFetchExampleTasks);
+
+export default store;
