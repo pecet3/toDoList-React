99c5f1a7a226dff46df2a613445c4f99f28e91aa
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-03 22:43:56 +0100

    changed effect

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,19 +1,20 @@
-import { takeEvery, call, put } from "redux-saga/effects";
-import { fetchExampleTasks, setTasks } from "./tasksSlice";
+import { takeLatest, call, put, delay, select } from "redux-saga/effects";
+import { fetchExampleTasks, setTasks, selectTasks } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTask";
 
 function* fetchExampleTasksHandler() {
     try {
+        yield delay(1000);
         const exampleTasks = yield call(getExampleTasks);
         yield put(setTasks(exampleTasks));
 
     } catch (error) {
-        yield call(alert, "coś poszło nie tak" );
+        yield call(alert, "coś poszło nie tak");
         yield call(console.error, error.message);
     }
-}
+};
 
 export function* watchFetchExampleTasks() {
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
-}
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+};
 
