e87f6bb7800a898b3e420293241086bed05498fd
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-03 22:43:20 +0100

    replaced saga path

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import tasksReducer from "./features/tasks/tasksSlice";
-import { watchFetchExampleTasks } from "./features/tasks/tasksSaga";
+import rootSaga from "./rootSaga";
 
 
 const sagaMiddleware = createSagaMiddleware();
@@ -13,6 +13,6 @@ const store = configureStore({
     middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(watchFetchExampleTasks);
+sagaMiddleware.run(rootSaga);
 
 export default store;
