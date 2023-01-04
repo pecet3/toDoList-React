e05dec9b4bf4745b66f28ce37f522f797ae46000
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-04 17:47:48 +0100

    renamed function

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,8 @@
 import { all } from "redux-saga/effects";
-import { watchFetchExampleTasks } from "./features/tasks/tasksSaga";
+import { tasksSaga } from "./features/tasks/tasksSaga";
 
 export default function* rootSaga() {
     yield all([
-        watchFetchExampleTasks(),
+        tasksSaga(),
     ]);
 }
