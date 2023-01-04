7ac03c6cb76461d6c8d6d1a8f60d908c3894b521
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-04 19:30:18 +0100

    created the file

---
+++ src/features/author/Author.js
@@ -0,0 +1,17 @@
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+
+const Author = () => (
+    <Container>
+        <Header
+            title="o Autorze"
+        />
+        <Section
+            title="Jakub Pacewicz"
+            body="Lorem ipsum dolor sit amet, consectetur... "
+        />
+    </Container>
+);
+
+export default Author;
