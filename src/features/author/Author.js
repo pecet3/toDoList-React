1efbe3c5eb5b1411c0cd19dc9c11998ec43bf07d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-04 20:42:14 +0100

    added content

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -5,11 +5,18 @@ import Container from "../../common/Container";
 const Author = () => (
     <Container>
         <Header
-            title="o Autorze"
+            title="O autorze"
         />
         <Section
             title="Jakub Pacewicz"
-            body="Lorem ipsum dolor sit amet, consectetur... "
+            body={
+                <>
+                    <p>Hej, nazywam się <b>Jakub Pacewicz</b>, mam 22 lata i pochodzę ze Szczytna.
+                        Pracuję w firmię Telus International, gdzie zajmuję się obsługą klienta.
+                        Po pracy gram na gitarze elektrycznej oraz <strong>uczę się frontendu!</strong>
+                        Czasem układam kostki rubika oraz inne łamigówki, gdyż kiedyś bardzo mnie to interesowało.</p>
+                </>
+            }
         />
     </Container>
 );
