107242a4c261edce7059da76161d64e87893cf8d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-04 19:30:08 +0100

    react-router navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
 const App = () => {
     return (
-        <Tasks />
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/zadania">
+                            Zadania
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/autor">
+                            Autor
+                        </Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/zadania">
+                        <Tasks />
+                    </Route>
+                    <Route path="/autor">
+                        <Author />
+                    </Route>
+                </Switch>
+            </nav>
+        </BrowserRouter>
     )
 };
 
