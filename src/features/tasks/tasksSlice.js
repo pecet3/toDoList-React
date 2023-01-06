015937ca857f4a2e282748b7a0ba388edc56137c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-06 16:33:21 +0100

    added new select

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -53,6 +53,17 @@ export const selectIsAllDone = state => selectTasksState(state).tasks.every(({ d
 export const selectTasksStateLenght = state => selectTasksState(state).tasks.length;
 
 export const getTaskById = (state, taskId) =>
-    selectTasks(state).find(({ id }) => id === taskId)
+    selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === '') {
+        return tasks;
+    }
+    return tasks.filter(({ content }) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
+
+
 
 export default tasksSlice.reducer;
