44088caacca8c4c22072fee679c4ef4e27d63bf3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-07 19:41:24 +0100

    style for content

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,4 +1,4 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 
 export const Container = styled.section`
@@ -8,16 +8,16 @@ export const Container = styled.section`
 `;
 
 export const HeaderContainer = styled.div`
-    border-bottom: 2px solid ${({theme})=> theme.colors.lightGrey};
+    border-bottom: 2px solid ${({ theme }) => theme.colors.lightGrey};
     padding: 15px;
     margin: 0 auto;
 
-    ${({buttons}) => buttons && css`
+    ${({ buttons }) => buttons && css`
     display: flex;
     justify-content: space-between;
     align-items: center;
 
-    @media (max-width: ${({theme})=> theme.breakpoints.mobile}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
         flex-direction: column;
         align-items: flex-start;
     }
@@ -32,6 +32,10 @@ export const Header = styled.h2`
 
 export const Content = styled.div`
     padding: 15px;
+    
+    ${({displayNone})=> displayNone && css`
+        display: none;
+    `};
 `;
 
 
