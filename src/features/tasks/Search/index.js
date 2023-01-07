c3c1fc4edc1c4c0a5723f7d7cf2f128fbec920f3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-07 19:42:30 +0100

    new component

--- src/features/tasks/Search/index.js
+++ src/features/tasks/Search/index.js
@@ -1,8 +1,27 @@
+import { useLocation, useHistory } from "react-router-dom";
+import { Input } from "../Input";
 
+const Search = ({search}) => {
+    const location = useLocation();
+    const history = useHistory()
+    const query = new URLSearchParams(location.search).get("szukaj");
 
-const Search = () => {
+    const onInputChange = ({target}) => {
+        const searchParams = new URLSearchParams(location.search)
+        if(target.value.trim() === ""){
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value);
+        }
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
     return (
-        <>nana</>
+        <Input
+            placeholder="Filtruj zadania"
+            value={query || ""}
+            onChange={onInputChange}
+            search = {search}
+        />
     )
 };
 
