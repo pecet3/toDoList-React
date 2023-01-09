c791ddba3290b4a87ddbad1f3def8992cfb82274
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-09 23:05:34 +0100

    replaced <nav> to separate component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
+import { Navigation } from "./Navigation";
+
 
 const App = () => {
     return (
         <HashRouter>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/zadania">
-                            Zadania
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/autor">
-                            Autor
-                        </Link>
-                    </li>
-                </ul>
+            <Navigation />
                 <Switch>
                     <Route path="/zadania/:id">
                         <TaskPage />
@@ -33,7 +23,6 @@ const App = () => {
                         <Redirect to="/zadania" />
                     </Route>
                 </Switch>
-            </nav>
         </HashRouter>
     )
 };
