388035f237dae22bd4bd29435be1ed2d96163f6f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-10 23:02:23 +0100

    flexbasis > width

--- src/features/tasks/Input/index.js
+++ src/features/tasks/Input/index.js
@@ -4,7 +4,7 @@ export const Input = styled.input`
     padding: 8px;
     
     ${({ search }) => search && css`
-        flex-basis: 75%;
+        width: 75%;
 
         @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
             margin-top: 10px;
