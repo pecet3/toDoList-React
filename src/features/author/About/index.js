5aab265724044ca97c32729e599be3642c937f91
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-13 00:11:20 +0100

    edited content in about

--- src/features/author/About/index.js
+++ src/features/author/About/index.js
@@ -4,7 +4,9 @@ export const About = () => (
     <Wrapper>
         <p>Hej, nazywam się <b>Jakub Pacewicz</b>, mam 22 lata i pochodzę ze Szczytna.
             Pracuję w firmię Telus International, gdzie zajmuję się obsługą klienta.
-            Po pracy gram na gitarze elektrycznej oraz <strong>uczę się frontendu!</strong>
-            Czasem układam kostki rubika oraz inne łamigówki, gdyż kiedyś bardzo mnie to interesowało.</p>
+            Po pracy gram na gitarze elektrycznej oraz <strong>uczę się frontendu</strong>,
+            dlatego stworzyłem tę listę zadań. Mam ogromną nadzieję, że moja aplikacja przyda Ci się 😀!</p>
+        <p>Jeśli masz jakieś propozycję lub uwagi to zapraszam do kontaktu.</p>
+        <p>Mój email: <a href="mailto:jakub@pacewicze.pl">jakub@pacewicze.pl</a></p>   
     </Wrapper>
 );
