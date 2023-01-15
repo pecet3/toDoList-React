3c9ec24e4089c8e43a24ccd4869fee6f2bee7d13
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-15 02:54:13 +0100

    created new slice

---
+++ src/Navigation/themeButtonSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { theme } from "../theme.js"
+
+const themeButtonSlice = createSlice({
+    name: "themeButton",
+    initialState: {
+        darkTheme: false,
+    },
+    reducers: {
+        toggleDarkTheme: (state) => {
+            state.darkTheme = !state.darkTheme;
+        },
+        toggleBackgroundColor: (state) => {
+            state.darkTheme ?
+                document.body.style.backgroundColor = theme.colors.mineShaft :
+                document.body.style.backgroundColor = theme.colors.lightGrey;
+        },
+        toggleColor: (state) => {
+            state.darkTheme ?
+                document.body.style.color = theme.colors.teal :
+                document.body.style.color = "black";
+        },
+    }
+});
+
+export const {
+    toggleDarkTheme,
+    toggleBackgroundColor,
+    toggleColor, } = themeButtonSlice.actions;
+
+export const selectDarkTheme = state => state.themeButton.darkTheme;
+
+export default themeButtonSlice.reducer;
