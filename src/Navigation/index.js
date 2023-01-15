187fb4ce7f155d7cf05d7aab3d6db8bce62fa5a2
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-15 02:53:46 +0100

    added theme button to navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,20 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
 import { toAuthor, toTasks } from "../routes";
-import { Li, Nav, StyledNavLink, Ul } from "./styled";
+import { selectDarkTheme, toggleDarkTheme } from "./themeButtonSlice";
+import {
+    Li,
+    Nav,
+    StyledNavLink,
+    Ul,
+    Button
+} from "./styled";
 
-export const Navigation = () => (
-    <Nav>
-    <Ul>
-        <Li>
-            <StyledNavLink to={toTasks()}>
-                Zadania
-            </StyledNavLink>
-        </Li>
-        <Li>
-            <StyledNavLink to={toAuthor()}>
-                Autor
-            </StyledNavLink>
-        </Li>
-    </Ul>
-    </Nav>
-);
+
+export const Navigation = () => {
+    const dispatch = useDispatch();
+    const darkTheme = useSelector(selectDarkTheme);
+    return (
+        <Nav>
+            <Ul>
+                <Li>
+                    <StyledNavLink to={toTasks()}>
+                        Zadania
+                    </StyledNavLink>
+                </Li>
+                <Li>
+                    <StyledNavLink to={toAuthor()}>
+                        Autor
+                    </StyledNavLink>
+                </Li>
+                <Li>
+                    <Button
+                        onClick={() => dispatch(toggleDarkTheme())}>
+                        {darkTheme ?
+                            "🌞 tryb jasny" :
+                            "🌙 tryb ciemny"}
+                    </Button>
+                </Li>
+            </Ul>
+        </Nav>
+    )
+};
 
